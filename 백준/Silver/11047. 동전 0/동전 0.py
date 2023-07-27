bad08585f1570925489959749df8412361ed0b47
MINJU KIM <demin1026@naver.com>
2023-07-28 00:31:55 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split(' '))
+arr = []
+count = 0
+
+for i in range(n):
+    a = int(input())
+    arr.append(a)
+
+
+for i in range(n-1,-1,-1):
+    if k%arr[i] != k:
+        count += k//arr[i]
+        k = k%arr[i]
+    if k == 1:
+        count += 1
+        break
+    if k == 0:
+        break
+print(count)
