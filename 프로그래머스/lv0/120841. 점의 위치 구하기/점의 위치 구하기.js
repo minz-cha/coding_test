a84a5f65620faa9d1118c6dce14c508ecf77d271
MINJU KIM <demin1026@naver.com>
2023-08-04 11:53:57 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,12 +1,5 @@
 function solution(dot) {
-    var answer = 0;
-    if (dot[0] > 0 && dot[1] > 0) {
-        return 1
-    } else if (dot[0] < 0 && dot[1] >0){
-        return 2
-    } else if (dot[0] < 0 && dot[1] <0){
-        return 3
-    } else{
-        return 4
-    }
+    const [num1, num2] = dot;
+    const check = num1 * num2 > 0;
+    return num1 > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
 }
