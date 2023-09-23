608afb5cab31af737567a6bf356e5173d7759f60
MINJU KIM <demin1026@naver.com>
2023-09-23 22:55:18 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,6 +1,25 @@
 function solution(a, b) {
-    var arr = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-    var date = new Date(`2016-${a}-${b}`);
-    var day = date.getDay()
-    return arr[day];
+    var days = 0
+    const month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    for (let i=0; i< a-1; i++){
+        days += month[i]
+    }
+    days += b
+    switch (days % 7){
+        case 1:
+            return "FRI"
+        case 2: 
+            return "SAT"
+        case 3:
+            return "SUN"
+        case 4:
+            return "MON"
+        case 5:
+            return "TUE"
+        case 6:
+            return "WED"
+        default:
+            return "THU"
+
+    }
 }
