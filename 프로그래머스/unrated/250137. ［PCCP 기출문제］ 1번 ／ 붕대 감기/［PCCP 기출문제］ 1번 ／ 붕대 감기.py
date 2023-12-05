6171b962e4b01c59ce4d288c38b64fe639e4a1b7
MINJU KIM <demin1026@naver.com>
2023-12-05 11:38:11 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 2.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -8,33 +8,37 @@ def control_health(num, max_):
         return num
 
 def solution(bandage, health, attacks):
-    current = health
-    count = 0
+    current = health # 체력 초깃값
+    count = 0 # 연속 성공
 
     for i in range(1, attacks[-1][0] + 1):
         current_attack = 0
-        attack_found = False
+        attack_found = False 
         
         for subattack in attacks:
-            if i == subattack[0]:
+            if i == subattack[0]: #시간 초와 공격시간 같은 경우
                 current_attack = subattack[1]
-                attack_found = True
+                attack_found = True # 공격 받음
                 break
     
+        # 공격 받은 경우
         if attack_found:
-            current -= current_attack
+            current -= current_attack 
             count = 0
             if current < 0 or current == 0:
                 return -1
 
+        # 공격 받지 않은 경우 - 회복
         else: 
             count += 1
+            # 연속 회복 성공
             if count == bandage[0]:
                 current = current + bandage[1] + bandage[2]
                 count = 0
             else:
                 current += bandage[1]
         
+        # 회복한 체력이 최대 체력을 넘지 않도록 제어
         current = control_health(current, health)
                 
     return current
